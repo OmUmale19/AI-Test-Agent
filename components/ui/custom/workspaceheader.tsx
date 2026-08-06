f0bba64bf597469993a49cf637021c4bda36bea0
OmUmale19 <omumale127@gmail.com>
2026-08-07 00:56:17 +0530

    Add SignedOut Sign In and Sign Up buttons to header

--- components/ui/custom/workspaceheader.tsx
+++ components/ui/custom/workspaceheader.tsx
@@ -1,4 +1,4 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -24,21 +24,36 @@ function Workspaceheader() {
                 </li>
             </ul>
 
-            {/* userbutton */}
+            {/* auth controls */}
             <div className='flex items-center gap-4'>
-                <UserButton
-                    appearance={{
-                        elements: {
-                            avatarBox: "!w-10 !h-10",
-                            userButtonAvatarImage: "!w-10 !h-10",
-                            userButtonTrigger: "!w-10 !h-10 focus:shadow-none"
-                        }
-                    }}
-                />
+                <SignedOut>
+                    <SignInButton mode="modal">
+                        <button className="px-4 py-2 text-sm font-medium rounded-lg bg-[#38bdf8] text-black hover:bg-sky-400 transition-all cursor-pointer">
+                            Sign In
+                        </button>
+                    </SignInButton>
+                    <SignUpButton mode="modal">
+                        <button className="px-4 py-2 text-sm font-medium rounded-lg border border-zinc-700 hover:bg-zinc-800 text-white transition-all cursor-pointer">
+                            Sign Up
+                        </button>
+                    </SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                avatarBox: "!w-10 !h-10",
+                                userButtonAvatarImage: "!w-10 !h-10",
+                                userButtonTrigger: "!w-10 !h-10 focus:shadow-none"
+                            }
+                        }}
+                    />
+                </SignedIn>
             </div>
         </div>
     )
 }
 
 export default Workspaceheader
+
 
